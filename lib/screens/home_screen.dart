6d7e84998876699ef09060b5d356d51a6590e580
yonatantasew <yonatantasew@gmail.com>
2022-04-18 16:57:53 +0300

    homescreen appbar layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,34 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text(
+          'Chats',
+          style: TextStyle(
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
   }
 }
