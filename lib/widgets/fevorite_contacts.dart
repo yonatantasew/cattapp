0cb3f82f857dd00411a2ca2278857f1d134f3e42
yoni <yonatantasew@gmail.com>
2022-04-20 22:39:44 +0300

    favorites index corrected

--- lib/widgets/fevorite_contacts.dart
+++ lib/widgets/fevorite_contacts.dart
@@ -9,46 +9,47 @@ class fevoriteContacts extends StatelessWidget {
   Widget build(BuildContext context) {
     //// this local variable is temporary solution for favorite[index] on gesture detector
     var index = 0;
-    return GestureDetector(
-      onTap: () => Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (_) => chatScreen(user: favorites[index]))),
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 10.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    'Fevorite Contacts',
-                    style: TextStyle(
-                        color: Colors.blueGrey,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1.0),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.more_horiz),
-                    color: Colors.blueGrey,
-                    iconSize: 30.0,
-                  )
-                ],
-              ),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10.0),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Fevorite Contacts',
+                  style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.more_horiz),
+                  color: Colors.blueGrey,
+                  iconSize: 30.0,
+                )
+              ],
             ),
-            Container(
-              height: 120.0,
-              // color: Colors.blue,
-              child: ListView.builder(
-                  padding: EdgeInsets.only(left: 10.0),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: favorites.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
+          ),
+          Container(
+            height: 120.0,
+            // color: Colors.blue,
+            child: ListView.builder(
+                padding: EdgeInsets.only(left: 10.0),
+                scrollDirection: Axis.horizontal,
+                itemCount: favorites.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) =>
+                                chatScreen(user: favorites[index]))),
+                    child: Padding(
                       padding: EdgeInsets.all(10.0),
                       child: Column(
                         children: [
@@ -69,11 +70,11 @@ class fevoriteContacts extends StatelessWidget {
                           ),
                         ],
                       ),
-                    );
-                  }),
-            )
-          ],
-        ),
+                    ),
+                  );
+                }),
+          )
+        ],
       ),
     );
   }
