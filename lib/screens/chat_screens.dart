58cd7921e6a396296f7dc7021024eb6f5e9a08d8
yonatantasew <yonatantasew@gmail.com>
2022-04-20 20:26:25 +0300

    chat screen messages list view

--- lib/screens/chat_screens.dart
+++ lib/screens/chat_screens.dart
@@ -1,3 +1,4 @@
+import 'package:chattapp/models/message_model.dart';
 import 'package:chattapp/models/user_model.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,55 @@ class chatScreen extends StatefulWidget {
 class _chatScreenState extends State<chatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        centerTitle: true,
+        title: Text(
+          widget.user.name,
+          style: TextStyle(
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_horiz),
+            iconSize: 30.0,
+            color: Colors.white,
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0)),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0)),
+                child: ListView.builder(
+                  padding: EdgeInsets.only(top: 15.0),
+                  itemCount: messages.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final Message messeg = messages[index];
+                    return Text(messages[index].text);
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/fevorite_contacts.dart
+++ lib/widgets/fevorite_contacts.dart
@@ -1,3 +1,4 @@
+import 'package:chattapp/screens/chat_screens.dart';
 import 'package:flutter/material.dart';
 import 'package:chattapp/models/message_model.dart';
 
@@ -6,65 +7,73 @@ class fevoriteContacts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 10.0),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  'Fevorite Contacts',
-                  style: TextStyle(
-                      color: Colors.blueGrey,
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1.0),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.more_horiz),
-                  color: Colors.blueGrey,
-                  iconSize: 30.0,
-                )
-              ],
+    //// this local variable is temporary solution for favorite[index] on gesture detector
+    var index = 0;
+    return GestureDetector(
+      onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (_) => chatScreen(user: favorites[index]))),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Fevorite Contacts',
+                    style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.0),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.more_horiz),
+                    color: Colors.blueGrey,
+                    iconSize: 30.0,
+                  )
+                ],
+              ),
             ),
-          ),
-          Container(
-            height: 120.0,
-            // color: Colors.blue,
-            child: ListView.builder(
-                padding: EdgeInsets.only(left: 10.0),
-                scrollDirection: Axis.horizontal,
-                itemCount: favorites.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 35.0,
-                          backgroundImage:
-                              AssetImage(favorites[index].imageUrl),
-                        ),
-                        SizedBox(
-                          height: 6.0,
-                        ),
-                        Text(
-                          favorites[index].name,
-                          style: TextStyle(
-                              color: Colors.blueGrey,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w600),
-                        ),
-                      ],
-                    ),
-                  );
-                }),
-          )
-        ],
+            Container(
+              height: 120.0,
+              // color: Colors.blue,
+              child: ListView.builder(
+                  padding: EdgeInsets.only(left: 10.0),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: favorites.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            radius: 35.0,
+                            backgroundImage:
+                                AssetImage(favorites[index].imageUrl),
+                          ),
+                          SizedBox(
+                            height: 6.0,
+                          ),
+                          Text(
+                            favorites[index].name,
+                            style: TextStyle(
+                                color: Colors.blueGrey,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
